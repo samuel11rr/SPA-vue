ed89eb1e2829ac12e819b9e81df41d51f8e294c0
Samuel Ramirez <samuel_ramirez@live.com.mx>
2021-04-26 13:25:13 -0500

    api communication added

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -6,8 +6,8 @@ const config = Object.assign({}, base, {
 })
 
 config.module.rules
-  .filter(x => {return x.loader == 'vue-loader'})
-  .forEach(x => x.options.extractCSS = true)
+  .filter(x => { return x.loader === 'vue-loader' })
+  .forEach(x => { x.options.extractCSS = true })
 
 config.plugins.push(
   new ExtractTextPlugin('assets/styles.css')
