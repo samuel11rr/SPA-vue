57336983f1022b62f7e8ba1c0c6f450d9e829ad7
Samuel Ramirez <samuel_ramirez@live.com.mx>
2021-04-28 23:22:11 -0500

    server side rendering

---
+++ src/server-entry.js
@@ -0,0 +1,14 @@
+import { app, router, store } from './app.js'
+
+export default context => {
+  router.push(context.url)
+
+  return Promise.all(router.getMatchedComponents().map(component => {
+    if (component.asyncData) {
+      return component.asyncData(store, router.currentRoute)
+    }
+  })).then(() => {
+    context.initialState = store.state
+    return app
+  })
+}
